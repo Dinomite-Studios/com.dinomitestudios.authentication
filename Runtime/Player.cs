53ff987e47de1d841f3b18b7ba0592b8ec1f8643
Dino Fejzagić <dino.f@live.de>
2024-03-21 21:09:33 +0100

    Remove username provider

--- Runtime/Player.cs
+++ Runtime/Player.cs
@@ -3,72 +3,22 @@ using UnityEngine;
 
 namespace Dinomite.Authentication
 {
+    /// <summary>
+    /// A player's information.
+    /// </summary>
     [Serializable]
     public class Player
     {
-        [SerializeField]
-        [Tooltip("The player's unique ID.")]
-        private string id;
-
         /// <summary>
         /// The player's unique ID.
         /// </summary>
-        public string Id
-        {
-            get => id;
-            set => id = value;
-        }
-
-        [SerializeField]
-        [Tooltip("The player's avatar source URL.")]
-        private string avatarUrl;
+        [field: SerializeField, Tooltip("The player's unique ID.")]
+        public string Id { get; set; }
 
         /// <summary>
-        /// The player's avatar source URL.
+        /// The player's in-game name.
         /// </summary>
-        public string AvatarUrl
-        {
-            get => avatarUrl;
-            set => avatarUrl = value;
-        }
-
-        [SerializeField]
-        [Tooltip("The country code of the player's country.")]
-        private string country;
-
-        /// <summary>
-        /// The country code of the player's country.
-        /// </summary>
-        public string Country
-        {
-            get => country;
-            set => country = value;
-        }
-
-        [SerializeField]
-        [Tooltip("The player's username.")]
-        private string username;
-
-        /// <summary>
-        /// The player's username.
-        /// </summary>
-        public string Username
-        {
-            get => username;
-            set => username = value;
-        }
-
-        [SerializeField]
-        [Tooltip("When was this player created?")]
-        private DateTime? createdAt;
-
-        /// <summary>
-        /// When was this player created?
-        /// </summary>
-        public DateTime? CreatedAt
-        {
-            get => createdAt;
-            set => createdAt = value;
-        }
+        [field: SerializeField, Tooltip("The player's username.")]
+        public string PlayerName { get; set; }
     }
 }
